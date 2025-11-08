531fcbcf2dfe9cc991cfb53c42d972d2efd7b11f
Mixa002 <mimi.djordjevic23@gmail.com>
2025-11-08 13:03:26 +0100

    dodato dodavanje i brisanje stavki kao i promene u comboboxu

--- ProjekatServer/src/serverskeOperacije/VratiSveIznajmljivanjaSO.java
+++ ProjekatServer/src/serverskeOperacije/VratiSveIznajmljivanjaSO.java
@@ -5,21 +5,44 @@
 package serverskeOperacije;
 
 import domain.AbstractDomainObject;
+import domain.Iznajmljivanje;
+import domain.StavkaIznajmljivanja;
+import java.util.ArrayList;
 
 /**
  *
  * @author mihajlodjordjevic
  */
-public class VratiSveIznajmljivanjaSO extends OpstaSO{
+public class VratiSveIznajmljivanjaSO extends OpstaSO {
 
     @Override
     public void proveriPreduslov(AbstractDomainObject ado) throws Exception {
-        
+
     }
 
     @Override
     protected Object izvrsiKonkretnuOperaciju(AbstractDomainObject ado) throws Exception {
-        return broker.select(ado);
+        ArrayList<AbstractDomainObject> listaADO = broker.select(ado);
+        ArrayList<Iznajmljivanje> iznajmljivanja = new ArrayList<>();
+        for (AbstractDomainObject obj : listaADO) {
+            iznajmljivanja.add((Iznajmljivanje) obj);
+        }
+
+        for (Iznajmljivanje iz : iznajmljivanja) {
+            StavkaIznajmljivanja si = new StavkaIznajmljivanja();
+            si.setIznajmljivanje(iz);
+
+            ArrayList<AbstractDomainObject> listaStavkiADO = broker.select(si);
+            ArrayList<StavkaIznajmljivanja> stavke = new ArrayList<>();
+
+            for (AbstractDomainObject obj : listaStavkiADO) {
+                stavke.add((StavkaIznajmljivanja) obj);
+            }
+
+            iz.setStavkeIznajmljivanja(stavke);
+        }
+
+        return iznajmljivanja;
     }
-    
+
 }
